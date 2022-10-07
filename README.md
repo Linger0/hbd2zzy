9ead8fce74cc2937a49dfc4bf78ea1b9c4bba54a
Linger <lingangliang21s@ict.ac.cn>
2022-10-07 20:30:48 +0800

    Save all files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Happy Birthday
+
+Celebrate her day by making a birthday card on the webpage.
+
+### Suggestion
+
+1. Write your wishes in `Memories.html`.
+2. Add a photo as background and modify the `background-url` in `css/memories.css`.
+
+### Info
+
+This repository does not include the backend, which is called in `AI.html`.
+The font files in `font` has been compressed with [Fomtmin](https://ecomfe.github.io/fontmin/).
---
+++ js/index.js
@@ -0,0 +1,59 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-25 09:07:37
+ * @LastEditTime: 2021-03-25 09:12:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ */
+$(document).ready(() => {
+    $("#main-page p").fadeIn(500);
+});
+$("#login-button").click(function (event) {
+    var userName = document.getElementById("userName").value;
+    // 修改密码请改此处
+    if (userName.toUpperCase().trim() == "TOT") {
+        event.preventDefault();
+        $("form").fadeOut(500);
+        $(".wrapper").addClass("form-success");
+        $(".container").fadeOut(500);
+        setTimeout(function () {
+            location.href = "Memories.html";
+        }, 500);
+    } else if (userName == "") {
+        event.preventDefault();
+    } else {
+        event.preventDefault();
+        alert("输错了！如果不是 TBD，请点返回进入另一个。");
+    }
+});
+$("#login-button-oth").click(function (event) {
+    var userName = document.getElementById("userName-oth").value;
+    // 修改密码请改此处
+    if (userName.toUpperCase().trim() == "1010") {
+        event.preventDefault();
+        $("form").fadeOut(500);
+        $(".wrapper").addClass("form-success");
+        $(".container").fadeOut(500);
+        setTimeout(function () {
+            location.href = "Memories-oth.html";
+        }, 500);
+    } else if (userName == "") {
+        event.preventDefault();
+    } else {
+        event.preventDefault();
+        alert("输错了！如果不知道口令，可以问 TBD，记得先送上生日祝福哦~");
+    }
+});
+$("#zzy-click").click(function () {
+    $("#main-page").fadeOut(500);
+    $("#zzy-login").fadeIn(500);
+});
+$("#oth-click").click(function () {
+    $("#main-page").fadeOut(500);
+    $("#oth-login").fadeIn(500);
+});
+$(".rtn-main").click(function () {
+    $("#zzy-login").fadeOut(500);
+    $("#oth-login").fadeOut(500);
+    $("#main-page").fadeIn(500);
+});
